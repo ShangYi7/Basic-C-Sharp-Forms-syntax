0676b497845fb2c5fb778ac159397d5d4a04853d
ShangYi7 <shangyi6.tw@gmail.com>
2023-10-22 01:29:46 +0800

    Updata annotation

--- 0921 計算價錢/Form1.cs
+++ 0921 計算價錢/Form1.cs
@@ -20,7 +20,7 @@ namespace _0921
         private void button1_Click(object sender, EventArgs e)
         {
             
-            int x1 = int.Parse(textBox1.Text);
+            int x1 = int.Parse(textBox1.Text); // textBox1 中的文字傳換成 int 儲存在 x1 中/The text in textBox1 is converted into int and stored in x1
             int x2 = int.Parse(textBox2.Text);
             label1.Text = $"買汽水 {x1} 瓶, 買沙士{x2} 瓶, 總共 {x1 * 20 + x2 * 25}";
         }
