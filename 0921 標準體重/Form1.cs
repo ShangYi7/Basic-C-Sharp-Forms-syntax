84a29a9f576fe749fa154615b469fd2a4824b55e
ShangYi7 <shangyi6.tw@gmail.com>
2023-10-22 01:33:46 +0800

    Updata annotation

--- 0921 標準體重/Form1.cs
+++ 0921 標準體重/Form1.cs
@@ -9,9 +9,11 @@ namespace _0921_標準體重
 
         private void button1_Click(object sender, EventArgs e)
         {
-            double x1;
-            x1 = double.Parse(textBox1.Text);
-            label1.Text = $"你的身高 {x1} cm, 你的標準體重 = {(x1 - 80) * 0.7:F2}";
+            
+            double x1 = double.Parse(textBox1.Text); // textBox1 中的文字轉換成 double 儲存在 x1 中/The text in textBox1 is converted into double and stored in x1
+            label1.Text = $"你的身高 {x1} cm, 你的標準體重 = {(x1 - 80) * 0.7:F2}"; // 是用 $ 直接帶入變數運算 /Use $ to directly bring in variable operations
+            // :F2  = 算到小數點第二位/Count to the second decimal place
+            // f = float
         }
     }
 }
